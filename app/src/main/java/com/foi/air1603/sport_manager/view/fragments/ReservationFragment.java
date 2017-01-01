2542b92de8b126e58700b3430c6e5b1146246708
Karlo Šimunović <karlo@oxidian.hr>
2017-01-01 22:00:42 +0100

    Refakturiran fragment, postavljen minimal date na kalendar

--- app/src/main/java/com/foi/air1603/sport_manager/view/fragments/ReservationFragment.java
+++ app/src/main/java/com/foi/air1603/sport_manager/view/fragments/ReservationFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.icu.util.Calendar;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,10 +14,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
-import android.widget.ImageView;
 import android.widget.Spinner;
-import android.widget.Switch;
-import android.widget.TextView;
 
 import com.foi.air1603.sport_manager.R;
 import com.foi.air1603.sport_manager.entities.Appointment;
@@ -29,7 +27,9 @@ import com.foi.air1603.sport_manager.view.ReservationView;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Korisnik on 26-Dec-16.
@@ -37,25 +37,27 @@ import java.util.List;
 
 public class ReservationFragment extends android.app.Fragment implements ReservationView {
 
+    static Map<String, Integer> inputs = new HashMap<>();
+
+    static {
+        inputs.put("appointmentLabel", R.id.tvReservationAppointmentLabel);
+        inputs.put("appointmentImage", R.id.reservationAppointmentImage);
+        inputs.put("sportImage", R.id.reservationSportImage);
+        inputs.put("playersImage", R.id.reservationPlayersImage);
+        inputs.put("spinnerAppointment", R.id.spinnerAppointments);
+        inputs.put("spinnerSport", R.id.spinnerSports);
+        inputs.put("spinnerMaxPlayers", R.id.spinnerPlayers);
+        inputs.put("privateSwitch", R.id.switchAppointmentPrivate);
+        inputs.put("setAppointmentButton", R.id.buttonSetAppointment);
+    }
+
     AppointmentPresenter appointmentPresenter;
     SportPresenter sportPresenter;
     CalendarView calendar;
-    List<String> maxPlayers = new ArrayList<String>();
-    private int yearGet;
-    private int monthGet;
-    private int dayGet;
-    private int id_place;
-    private int currentPickedDate;
+    List<String> maxPlayers = new ArrayList<>();
+    private int yearGet, monthGet, dayGet, id_place, currentPickedDate;
     private Place place;
-    private TextView appointmentLabel;
-    private ImageView appointmentImage;
-    private ImageView sportImage;
-    private ImageView playersImage;
-    private Spinner spinnerAppointment;
-    private Spinner spinnerSport;
-    private Spinner spinnerMaxPlayers;
-    private Switch privateSwitch;
-    private Button setAppointmentButton;
+    private Spinner spinnerAppointment, spinnerSport, spinnerMaxPlayers;
     private View view;
     private Context context;
 
@@ -76,39 +78,41 @@ public class ReservationFragment extends android.app.Fragment implements Reserva
         return v;
     }
 
+    private void showAllViews(Boolean show) {
+        for (Map.Entry<String, Integer> entry : inputs.entrySet()) {
+            View tmpView = view.findViewById(entry.getValue());
+            if (tmpView != null) {
+                if (show) {
+                    tmpView.setVisibility(View.VISIBLE);
+                } else {
+                    tmpView.setVisibility(View.GONE);
+                }
+            }
+        }
+    }
+
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         this.view = view;
-        calendar = (CalendarView) view.findViewById(R.id.calendarViewReservation);
 
-        //appointmentPresenter = new AppointmentPresenterImpl(this);
+        calendar = (CalendarView) view.findViewById(R.id.calendarViewReservation);
+        calendar.setFirstDayOfWeek(2);
+        calendar.setMinDate(System.currentTimeMillis() - 1000);
+
         appointmentPresenter = AppointmentPresenterImpl.getInstance().Init(this);
         appointmentPresenter.loadAllAppointments();
 
-
-
-        // sets the first day of week according to Calendar.
-        calendar.setFirstDayOfWeek(2);
-
         sportPresenter = new SportPresenterImpl(this);
 
-        spinnerAppointment = (Spinner) view.findViewById(R.id.spinnerAppointments);
-        spinnerAppointment.setVisibility(View.GONE);
-        appointmentLabel = (TextView) view.findViewById(R.id.tvReservationAppointmentLabel);
-        appointmentLabel.setVisibility(View.GONE);
-        appointmentImage = (ImageView) view.findViewById(R.id.reservationAppointmentImage);
-        appointmentImage.setVisibility(View.GONE);
-        sportImage = (ImageView) view.findViewById(R.id.reservationSportImage);
-        sportImage.setVisibility(View.GONE);
-        playersImage = (ImageView) view.findViewById(R.id.reservationPlayersImage);
-        playersImage.setVisibility(View.GONE);
-        privateSwitch = (Switch) view.findViewById(R.id.switchAppointmentPrivate);
-        privateSwitch.setVisibility(View.GONE);
-        setAppointmentButton = (Button) view.findViewById(R.id.buttonSetAppointment);
-        setAppointmentButton.setVisibility(View.GONE);
+        showAllViews(false);
 
+        spinnerAppointment = (Spinner) view.findViewById(R.id.spinnerAppointments);
+        spinnerSport = (Spinner) view.findViewById(R.id.spinnerSports);
+        spinnerMaxPlayers = (Spinner) view.findViewById(R.id.spinnerPlayers);
+
+        Button setAppointmentButton = (Button) view.findViewById(R.id.buttonSetAppointment);
         setAppointmentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -122,40 +126,24 @@ public class ReservationFragment extends android.app.Fragment implements Reserva
         spinnerAppointment.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                spinnerMaxPlayers.setVisibility(View.VISIBLE);
-                spinnerSport.setVisibility(View.VISIBLE);
-                appointmentLabel.setVisibility(View.VISIBLE);
-                appointmentImage.setVisibility(View.VISIBLE);
-                sportImage.setVisibility(View.VISIBLE);
-                sportImage.setVisibility(View.VISIBLE);
-                playersImage.setVisibility(View.VISIBLE);
-                privateSwitch.setVisibility(View.VISIBLE);
-                setAppointmentButton.setVisibility(View.VISIBLE);
+                showAllViews(true);
 
                 int max = Integer.parseInt(maxPlayers.get(position));
-
-                List<Integer> maxPly = new ArrayList<Integer>();
-
+                List<Integer> maxPly = new ArrayList<>();
                 for (int i = 1; i <= max; i++) {
                     maxPly.add(i);
                 }
 
-                ArrayAdapter<Integer> dataAdapter = new ArrayAdapter<Integer>(context, android.R.layout.simple_spinner_item, maxPly);
+                ArrayAdapter<Integer> dataAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, maxPly);
                 dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 spinnerMaxPlayers.setAdapter(dataAdapter);
-
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parentView) {
                 // your code here
             }
-
         });
-        spinnerSport = (Spinner) view.findViewById(R.id.spinnerSports);
-        spinnerSport.setVisibility(View.GONE);
-        spinnerMaxPlayers = (Spinner) view.findViewById(R.id.spinnerPlayers);
-        spinnerMaxPlayers.setVisibility(View.GONE);
         sportPresenter.getMultipleSports();
     }
 
@@ -169,14 +157,12 @@ public class ReservationFragment extends android.app.Fragment implements Reserva
         c.set(Calendar.SECOND, 0);
         c.set(Calendar.MILLISECOND, 0);
         currentPickedDate = (int) (c.getTimeInMillis() / 1000); //Today
-        calendar.setMinDate((int) (c.getTimeInMillis() / 1000));
-        System.out.println("testtttttttt:vrijeme:"+calendar.getMinDate());
 
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
 
             @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int day) {
                 //date in unix
                 yearGet = year;
                 monthGet = month;
@@ -186,23 +172,11 @@ public class ReservationFragment extends android.app.Fragment implements Reserva
                     return;
                 }
 
-                maxPlayers.clear();
-
-                spinnerAppointment.setVisibility(View.GONE);
-                spinnerSport.setVisibility(View.GONE);
-                spinnerMaxPlayers.setVisibility(View.GONE);
-                appointmentLabel.setVisibility(View.GONE);
-                appointmentImage.setVisibility(View.GONE);
-                sportImage.setVisibility(View.GONE);
-                sportImage.setVisibility(View.GONE);
-                playersImage.setVisibility(View.GONE);
-                privateSwitch.setVisibility(View.GONE);
-                setAppointmentButton.setVisibility(View.GONE);
+                showAllViews(false);
 
                 currentPickedDate = getDate();
                 appointmentPresenter.showAppointmentsForDate(getDate());
             }
-
         });
         appointmentPresenter.showAppointmentsForDate(currentPickedDate);
     }
@@ -211,10 +185,10 @@ public class ReservationFragment extends android.app.Fragment implements Reserva
     @RequiresApi(api = Build.VERSION_CODES.N)
     public int getDate() {
         Calendar c = Calendar.getInstance();
+
         c.set(Calendar.YEAR, yearGet);
         c.set(Calendar.MONTH, monthGet);
         c.set(Calendar.DAY_OF_MONTH, dayGet);
-
         c.set(Calendar.HOUR_OF_DAY, 0);
         c.set(Calendar.MINUTE, 0);
         c.set(Calendar.SECOND, 0);
@@ -232,30 +206,14 @@ public class ReservationFragment extends android.app.Fragment implements Reserva
     @Override
     public void showAppointmentsForDate(List<Appointment> appointments) {
         if (appointments.size() == 0) {
-            spinnerAppointment.setVisibility(View.GONE);
-            spinnerSport.setVisibility(View.GONE);
-            appointmentLabel.setVisibility(View.GONE);
-            appointmentImage.setVisibility(View.GONE);
-            sportImage.setVisibility(View.GONE);
-            sportImage.setVisibility(View.GONE);
-            playersImage.setVisibility(View.GONE);
-            privateSwitch.setVisibility(View.GONE);
-            setAppointmentButton.setVisibility(View.GONE);
+            showAllViews(false);
             return;
         }
 
-        spinnerAppointment.setVisibility(View.VISIBLE);
-        spinnerSport.setVisibility(View.VISIBLE);
-        appointmentLabel.setVisibility(View.VISIBLE);
-        appointmentImage.setVisibility(View.VISIBLE);
-        sportImage.setVisibility(View.VISIBLE);
-        sportImage.setVisibility(View.VISIBLE);
-        playersImage.setVisibility(View.VISIBLE);
-        privateSwitch.setVisibility(View.VISIBLE);
-        setAppointmentButton.setVisibility(View.VISIBLE);
+        showAllViews(true);
+        maxPlayers.clear();
 
         List<String> spinnerAppointments = new ArrayList<>();
-
         for (Appointment appointment : appointments) {
             spinnerAppointments.add(appointment.start + "-" + appointment.end);
             maxPlayers.add(appointment.maxplayers.toString());
@@ -265,18 +223,17 @@ public class ReservationFragment extends android.app.Fragment implements Reserva
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerAppointment.setAdapter(dataAdapter);
     }
+
     @Override
     public void showSports(List<Integer> id, List<String> name) {
 
-        List<String> sports = new ArrayList<String>();
+        List<String> sports = new ArrayList<>();
         for (int i = 0; i < id.size(); i++) {
             sports.add(name.get(i));
         }
 
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, sports);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, sports);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerSport.setAdapter(dataAdapter);
     }
-
-
 }
