5ab2bf1fafe5ee8cbce69b4f9406909631d94745
Marko Flajšek <marko.flajsek@gmail.com>
2017-01-02 22:05:58 +0100

    Popravljen dizajn screena za prikaz rezerviranih termina objekta

--- app/src/main/java/com/foi/air1603/sport_manager/adapters/MyPlaceReservationsRecycleAdapter.java
+++ app/src/main/java/com/foi/air1603/sport_manager/adapters/MyPlaceReservationsRecycleAdapter.java
@@ -5,10 +5,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.foi.air1603.sport_manager.R;
 import com.foi.air1603.sport_manager.entities.Appointment;
+import com.foi.air1603.sport_manager.entities.Reservation;
 import com.foi.air1603.sport_manager.view.fragments.MyPlacesReservationFragment;
 
 import java.util.List;
@@ -21,10 +23,12 @@ public class MyPlaceReservationsRecycleAdapter extends RecyclerView.Adapter<Recy
     MyPlacesReservationFragment myPlacesReservationFragment;
     Context context;
     List<Appointment> appointmentList;
+    //List<Reservation> reservationList;
 
     public MyPlaceReservationsRecycleAdapter(MyPlacesReservationFragment myPlacesReservationFragment, List<Appointment> appointmentList) {
         this.myPlacesReservationFragment = myPlacesReservationFragment;
         this.appointmentList = appointmentList;
+        //this.reservationList = reservationList;
     }
 
     @Override
@@ -38,9 +42,12 @@ public class MyPlaceReservationsRecycleAdapter extends RecyclerView.Adapter<Recy
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         final Appointment appointment = appointmentList.get(position);
+        //final Reservation reservation = reservationList.get(position);
+
         ((MyPlaceReservationsViewHolder) holder).place_appointment_date.setText(appointment.date);
         ((MyPlaceReservationsViewHolder) holder).place_appointment_start.setText(appointment.start);
         ((MyPlaceReservationsViewHolder) holder).place_appointment_end.setText(appointment.end);
+        //((MyPlaceReservationsViewHolder) holder).place_appointment_sport_image.setImageResource(reservation.);
     }
 
     @Override
@@ -53,11 +60,26 @@ public class MyPlaceReservationsRecycleAdapter extends RecyclerView.Adapter<Recy
         TextView place_appointment_start;
         TextView place_appointment_end;
 
+        /*
+        ImageView place_appointment_sport_image;
+        TextView place_appointment_sport_name;
+        TextView place_appointment_main_user;
+        TextView place_appointment_other_users;
+        TextView place_appointment_id;
+        */
+
         public MyPlaceReservationsViewHolder(View view) {
             super(view);
             place_appointment_date = (TextView) view.findViewById(R.id.place_appointment_date);
             place_appointment_start = (TextView) view.findViewById(R.id.place_appointment_start);
             place_appointment_end = (TextView) view.findViewById(R.id.place_appointment_end);
+            /*
+            place_appointment_sport_image = (ImageView) view.findViewById(R.id.place_appointment_image);
+            place_appointment_sport_name = (TextView) view.findViewById(R.id.place_appointment_sport_name);
+            place_appointment_main_user = (TextView) view.findViewById(R.id.place_appointment_user_main);
+            place_appointment_other_users = (TextView) view.findViewById(R.id.place_appointment_other_users);
+            place_appointment_id = (TextView) view.findViewById(R.id.place_appointment_id);
+            */
         }
     }
 }
